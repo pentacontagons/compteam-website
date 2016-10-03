8bd72c065b72e8b13746ca4c5716d56bd6f29aeb
pentacontagons <pentacontagons@gmail.com>
2016-10-03 13:52:40 -0400

    Added links to forensics and web challenges

--- meetings/index.php
+++ meetings/index.php
@@ -6,6 +6,10 @@
 
   <body>
     <h1> <a href="files/October_4.pdf"> October 4th presentation </a> </h1>
+    
+    <h3> <a href="files/nocomment.html"> Web Challenge </a> </h3>
+    
+    <h3> <a href="files/grey-aliens.png"> Forensics Challenge </a> </h3> 
   </body>
 
 </html>

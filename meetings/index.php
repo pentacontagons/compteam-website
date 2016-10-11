63fa612250ebe7dace869cd7c4b9c5960844cebc
pentacontagons <pentacontagons@gmail.com>
2016-10-11 09:20:22 -0400

    Changed meeting

--- meetings/index.php
+++ meetings/index.php
@@ -2,17 +2,43 @@
 
   <head>
     <title> Meeting Details </title>
+	<link rel="stylesheet" type="text/css" href="stylesheet.css"> 
   </head>
 
   <body>
-    
-    <h1> <a href="files/October_11.pdf"> October 11th Presentation </a> </h1>
-    
-    <h1> <a href="files/October_4.pdf"> October 4th Presentation </a> </h1>
-    
-    <h3> <a href="files/nocomment.html"> Web Challenge </a> </h3>
-    
-    <h3> <a href="files/grey-aliens.png"> Forensics Challenge </a> </h3> 
+    		<div id="wrapper">
+
+			<div class="header">
+
+
+				<img src="../logo.png" alt="Logo" class="logo">
+				<hr>
+				<ul>
+
+					<li> <a href="../"> Home </a> </li> 
+					<li class="current"> <a href="/"> Meetings </a> </li>
+					<li> <a href="/"> Calendar </a> </li>
+					<li> <a href="/"> Write Ups </a> </li> 
+	
+				</ul>
+				<hr width="1" size="500">
+			
+			</div>
+	
+			<div class="info">
+	
+				<h1> <a href="files/October_11.pdf"> October 11th Presentation </a> </h1>
+				
+				<h1> <a href="files/October_4.pdf"> October 4th Presentation </a> </h1>
+				
+				<h3> <a href="files/nocomment.html"> Web Challenge </a> </h3>
+				
+				<h3> <a href="files/grey-aliens.png"> Forensics Challenge </a> </h3> 
+
+	
+			</div>
+
+		</div>
   </body>
 
 </html>

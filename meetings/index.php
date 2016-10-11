5fabdfab4546731d206afa624959a5d4d42d77bf
pentacontagons <pentacontagons@gmail.com>
2016-10-10 21:41:51 -0400

    October 11

--- meetings/index.php
+++ meetings/index.php
@@ -5,7 +5,10 @@
   </head>
 
   <body>
-    <h1> <a href="files/October_4.pdf"> October 4th presentation </a> </h1>
+    
+    <h1> <a href="files/October_11.pdf"> October 11th Presentation </a> </h1>
+    
+    <h1> <a href="files/October_4.pdf"> October 4th Presentation </a> </h1>
     
     <h3> <a href="files/nocomment.html"> Web Challenge </a> </h3>
     
